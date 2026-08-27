80b59ee00f805931f5a7240aa5affd40dd021214
Josh Hug <hug@cs.berkeley.edu>
2026-08-26 17:06:20 -0700

    day 1 code

---
+++ lec1_intro1/HelloNumbers.java
@@ -0,0 +1,14 @@
+void main() {
+	// before we can use a variable
+	// in Java, we must declare it
+	// we'll always do so by
+	// saying its type and then its name 
+	// and then a semi-colon
+	int x;
+
+	x = 0;
+	while (x < 10) {
+		IO.println(x);
+		x = x + 1;		
+	}
+}
---
+++ lec1_intro1/LargerDemo.java
@@ -0,0 +1,12 @@
+int larger(int x, int y) {
+	if (x > y) {
+		return x;
+	}	
+	return y;
+}
+
+void main() {
+	IO.println(larger(5, 10));
+	IO.println(larger("a", "z"));
+}
+	
