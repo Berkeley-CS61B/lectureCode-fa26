ae63a02001f283fe74b6e740782bd835245776db
Josh Hug <hug@cs.berkeley.edu>
2026-09-14 13:46:53 -0700

    lecture 8 code (before lecture)

---
+++ lec8_lists4/AList.java
@@ -0,0 +1,41 @@
+package lec8_lists4;
+
+// AList is an infinitely extensible list of intege3rs
+// that uses an array under the hood.
+public class AList {
+    public int[] items;
+    public int size;
+
+    public AList() {
+        items = new int[11];
+        size = 0;
+    }
+
+    /** returns number of items in the list */
+    public int size() {
+        return size;
+    }
+
+    // adds an item to the end
+    public void addLast(int x) {
+        // items: [0 0 0 0 0 0 0 0 0 0 0 0]
+        // size: 0
+        items[size] = x;
+        size += 1;
+    }
+    public void addLast(int x) {
+        if (__________________________) {
+            int[] resized = new int[size + 1];
+            for (_______________________________________) {
+                ______________ = ______________;
+            }
+            __________ = resized;
+        }
+        items[size] = x;
+        size += 1;
+    }
+    /** returns the ith item */
+    public int get(int i) {
+        return items[i];
+    }
+}
